8e67a7befd43337fa9cb8ccb7498dbf0430c3678
Noah Morton <noahmorton98@gmail.com>
2017-01-24 17:11:10 -0600

    Added a small temporary file for testing git with.

---
+++ src/pack1/GenericTestFile.java
@@ -0,0 +1,14 @@
+package pack1;
+
+/**
+ * A simple test file for introducing a partner to git.
+ *
+ * @author Noah Morton
+ *
+ * Date created: Jan 24, 2017
+ *
+ * Part of project: TicTacToeAI
+ */
+public class GenericTestFile {
+
+}
