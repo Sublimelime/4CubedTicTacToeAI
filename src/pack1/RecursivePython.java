ae3413609507d8f6f6254999e4417bc99ebd8952
NoahNMorton <noahmorton98@gmail.com>
2017-01-25 14:20:01 -0600

    Started work on AI

---
+++ src/pack1/RecursivePython.java
@@ -0,0 +1,31 @@
+package pack1;
+
+import tully.*;
+
+/**
+ * An AI coded by Hunter Wright and Noah Morton
+ *
+ * @author Noah Morton/Hunter Wright
+ *
+ * Date created: Jan 25, 2017
+ *
+ * Part of project: TicTacToeAI
+ */
+public class RecursivePython implements PlayerInt {
+
+    @Override
+    public char getLetter() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public LocationInt getMove(BoardInt board) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
