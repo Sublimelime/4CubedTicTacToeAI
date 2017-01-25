502ac1eb09be8e0212147679c9b2e40c6e9bc779
hunterwright <wright.hunter22@gmail.com>
2017-01-25 13:55:56 -0600

    Changed author name and added int var.

--- src/pack1/GenericTestFile.java
+++ src/pack1/GenericTestFile.java
@@ -3,12 +3,12 @@ package pack1;
 /**
  * A simple test file for introducing a partner to git.
  *
- * @author Noah Morton
+ * @author Hunter Morton
  *
  * Date created: Jan 24, 2017
  *
  * Part of project: TicTacToeAI
  */
 public class GenericTestFile {
-
+    int i = 0;
 }
