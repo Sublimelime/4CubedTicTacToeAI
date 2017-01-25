4deb8c1a23605e0166add60c55c8c8a2a361854c
hunterwright <wright.hunter22@gmail.com>
2017-01-25 14:23:36 -0600

    Added constructor & fixed getMethod()

--- src/pack1/RecursivePython.java
+++ src/pack1/RecursivePython.java
@@ -5,17 +5,22 @@ import tully.*;
 /**
  * An AI coded by Hunter Wright and Noah Morton
  *
- * @author Noah Morton/Hunter Wright
- *
- * Date created: Jan 25, 2017
- *
- * Part of project: TicTacToeAI
+ * @author Noah Morton/Hunter Wright Date created: Jan 25, 2017 Part of project:
+ *         TicTacToeAI
  */
 public class RecursivePython implements PlayerInt {
 
+    private char letter;
+    private String name;
+
+    private RecursivePython(char letter) {
+        this.letter = letter;
+        this.name = "Recursive Python";
+    }
+
     @Override
     public char getLetter() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return letter;
     }
 
     @Override
