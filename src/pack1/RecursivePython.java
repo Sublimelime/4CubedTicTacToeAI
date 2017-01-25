c45c5ec04043a7c72b5aa8c2d71d6322d2115485
NoahNMorton <noahmorton98@gmail.com>
2017-01-25 14:30:04 -0600

    Added javadoc, fixed getName, cleared out getMove

--- src/pack1/RecursivePython.java
+++ src/pack1/RecursivePython.java
@@ -6,7 +6,7 @@ import tully.*;
  * An AI coded by Hunter Wright and Noah Morton
  *
  * @author Noah Morton/Hunter Wright Date created: Jan 25, 2017 Part of project:
- *         TicTacToeAI
+ * TicTacToeAI
  */
 public class RecursivePython implements PlayerInt {
 
@@ -23,14 +23,20 @@ public class RecursivePython implements PlayerInt {
         return letter;
     }
 
+    /**
+     * Gets the move from the AI, sends it to the parent.
+     *
+     * @param board The board provided by the calling class.
+     * @return returns the move that the AI selected.
+     */
     @Override
     public LocationInt getMove(BoardInt board) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return null;
     }
 
     @Override
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return name;
     }
 
 }
