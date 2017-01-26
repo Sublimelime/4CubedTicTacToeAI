fe022d5907cba3f8ed3c50ad15b864f0bbfab2d8
NoahNMorton <noahmorton98@gmail.com>
2017-01-26 13:51:53 -0600

    Fixed issue with javadoc, made constructor public

--- src/pack1/RecursivePython.java
+++ src/pack1/RecursivePython.java
@@ -5,15 +5,16 @@ import tully.*;
 /**
  * An AI coded by Hunter Wright and Noah Morton
  *
- * @author Noah Morton/Hunter Wright Date created: Jan 25, 2017 Part of project:
- * TicTacToeAI
+ * @author Noah Morton/Hunter Wright
+ *
+ * Date created: Jan 25, 2017 Part of project: TicTacToeAI
  */
 public class RecursivePython implements PlayerInt {
 
     private char letter;
     private String name;
 
-    private RecursivePython(char letter) {
+    public RecursivePython(char letter) {
         this.letter = letter;
         this.name = "Recursive Python";
     }
