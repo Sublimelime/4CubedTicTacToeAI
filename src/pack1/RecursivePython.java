73e3deb985e3eefc6f6fd67998415342e7332dd5
hunterwright <wright.hunter22@gmail.com>
2017-01-31 14:27:23 -0600

    Optimized Arraylists for saving locations

--- src/pack1/RecursivePython.java
+++ src/pack1/RecursivePython.java
@@ -9,7 +9,7 @@ import java.util.Random;
  * An AI coded by Hunter Wright and Noah Morton
  *
  * @author Noah Morton/Hunter Wright Date created: Jan 25, 2017 Part of project:
- * TicTacToeAI
+ *         TicTacToeAI
  */
 public class RecursivePython implements PlayerInt {
 
@@ -44,11 +44,17 @@ public class RecursivePython implements PlayerInt {
         Location locFinal = null;
 
         Random rand = new Random();
-        ArrayList<Location> zeros = new ArrayList<Location>();
-        ArrayList<Location> singles = new ArrayList<Location>();
-        ArrayList<Location> doubles = new ArrayList<Location>();
-        ArrayList<Location> triples = new ArrayList<Location>();
-        ArrayList<Location> quadruples = new ArrayList<Location>();
+        ArrayList<Location> selfZeros = new ArrayList<Location>();
+        ArrayList<Location> selfSingles = new ArrayList<Location>();
+        ArrayList<Location> selfDoubles = new ArrayList<Location>();
+        ArrayList<Location> selfTriples = new ArrayList<Location>();
+        ArrayList<Location> selfQuadruples = new ArrayList<Location>();
+
+        ArrayList<Location> otherZeros = new ArrayList<Location>();
+        ArrayList<Location> otherSingles = new ArrayList<Location>();
+        ArrayList<Location> otherDoubles = new ArrayList<Location>();
+        ArrayList<Location> otherTriples = new ArrayList<Location>();
+        ArrayList<Location> otherQuadruples = new ArrayList<Location>();
 
         for (int sheet = 0; sheet < board.numSheets(); sheet++) { //Go through all the spots on the board
             for (int row = 0; row < board.numRows(); row++) {
@@ -58,16 +64,28 @@ public class RecursivePython implements PlayerInt {
 
                         LocationScore ls = new LocationScore(board, locCurrent, letter);
 
-                        if (ls.getSelfQuadruples() > 0) {
-                            quadruples.add(new Location(sheet, row, col));
-                        } else if (ls.getSelfTriples() > 0) {
-                            triples.add(new Location(sheet, row, col));
-                        } else if (ls.getSelfDoubles() > 0) {
-                            doubles.add(new Location(sheet, row, col));
-                        } else if (ls.getSelfSingles() > 0) {
-                            singles.add(new Location(sheet, row, col));
+                        if (ls.getOtherQuadruples() > 0) {
+                            otherQuadruples.add(new Location(sheet, row, col));
+                        } else if (ls.getOtherTriples() > 0) {
+                            otherTriples.add(new Location(sheet, row, col));
+                        } else if (ls.getOtherDoubles() > 0) {
+                            otherDoubles.add(new Location(sheet, row, col));
+                        } else if (ls.getOtherSingles() > 0) {
+                            otherSingles.add(new Location(sheet, row, col));
                         } else {
-                            zeros.add(new Location(sheet, row, col));
+                            otherZeros.add(new Location(sheet, row, col));
+                        }
+
+                        if (ls.getSelfQuadruples() > 0) {
+                            selfQuadruples.add(new Location(sheet, row, col));
+                        } else if (ls.getSelfTriples() > 0) {
+                            selfTriples.add(new Location(sheet, row, col));
+                        } else if (ls.getSelfDoubles() > 0) {
+                            selfDoubles.add(new Location(sheet, row, col));
+                        } else if (ls.getSelfSingles() > 0) {
+                            selfSingles.add(new Location(sheet, row, col));
+                        } else {
+                            selfZeros.add(new Location(sheet, row, col));
                         }
 
                         score = (ls.getSelfQuadruples() * 1000) + (ls.getSelfTriples() * 100) + (ls.getSelfDoubles() * 10) + ls.getSelfSingles();
@@ -81,20 +99,6 @@ public class RecursivePython implements PlayerInt {
                     }
                 }
             }
-        }
-
-        if (quadruples.size() > 0) {
-            locFinal = quadruples.get(rand.nextInt(quadruples.size()));
-        } else if (triples.size() > 0) {
-            locFinal = triples.get(rand.nextInt(triples.size()));
-        } else if (doubles.size() > 0) {
-            locFinal = doubles.get(rand.nextInt(doubles.size()));
-        } else if (singles.size() > 0) {
-            locFinal = singles.get(rand.nextInt(singles.size()));
-        } else if (zeros.size() > 0) {
-            locFinal = zeros.get(rand.nextInt(zeros.size()));
-        } else {
-            locFinal = new Location(0, 0, 0);
         }
 
         //System.out.println("I moved to..." + loc);
