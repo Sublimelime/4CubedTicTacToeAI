879c047b4d9e8ad5e4dc5bb21a4af44863c2f4b8
Noah Morton <noahmorton98@gmail.com>
2017-01-31 20:48:41 -0600

    Trying again, seems that didn't work. Previous commit msg

--- src/pack1/RecursivePython.java
+++ src/pack1/RecursivePython.java
@@ -1,22 +1,38 @@
 package pack1;
 
-import tully.*;
-
 import java.util.ArrayList;
 import java.util.Random;
+import tully.*;
 
 /**
  * An AI coded by Hunter Wright and Noah Morton
  *
  * @author Noah Morton/Hunter Wright Date created: Jan 25, 2017 Part of project:
- *         TicTacToeAI
+ * TicTacToeAI
  */
 public class RecursivePython implements PlayerInt {
 
-    private char letter;
-    private String name;
-    private boolean movedRandomly = false;
+    private final char letter;
+    private final String name;
 
+    //location arraylists
+    ArrayList<Location> selfZeros;
+    ArrayList<Location> selfSingles;
+    ArrayList<Location> selfDoubles;
+    ArrayList<Location> selfTriples;
+    ArrayList<Location> selfQuadruples;
+
+    ArrayList<Location> otherZeros;
+    ArrayList<Location> otherSingles;
+    ArrayList<Location> otherDoubles;
+    ArrayList<Location> otherTriples;
+    ArrayList<Location> otherQuadruples;
+
+    /**
+     * Creates a new instance of the RecursivePython AI.
+     *
+     * @param letter The letter the AI will be playing as.
+     */
     public RecursivePython(char letter) {
         this.letter = letter;
         this.name = "Recursive Python";
@@ -40,21 +56,22 @@ public class RecursivePython implements PlayerInt {
      */
     @Override
     public LocationInt getMove(BoardInt board) {
-        int score, scoreTemp = 0, otherScore;
-        Location locFinal = null;
+        int score, scoreTemp = 0;
+        Location locFinal = null; //holds the location to be returned at the end.
 
         Random rand = new Random();
-        ArrayList<Location> selfZeros = new ArrayList<Location>();
-        ArrayList<Location> selfSingles = new ArrayList<Location>();
-        ArrayList<Location> selfDoubles = new ArrayList<Location>();
-        ArrayList<Location> selfTriples = new ArrayList<Location>();
-        ArrayList<Location> selfQuadruples = new ArrayList<Location>();
+        //clear and recreate all the arraylists
+        selfZeros = new ArrayList<>();
+        selfSingles = new ArrayList<>();
+        selfDoubles = new ArrayList<>();
+        selfTriples = new ArrayList<>();
+        selfQuadruples = new ArrayList<>();
 
-        ArrayList<Location> otherZeros = new ArrayList<Location>();
-        ArrayList<Location> otherSingles = new ArrayList<Location>();
-        ArrayList<Location> otherDoubles = new ArrayList<Location>();
-        ArrayList<Location> otherTriples = new ArrayList<Location>();
-        ArrayList<Location> otherQuadruples = new ArrayList<Location>();
+        otherZeros = new ArrayList<>();
+        otherSingles = new ArrayList<>();
+        otherDoubles = new ArrayList<>();
+        otherTriples = new ArrayList<>();
+        otherQuadruples = new ArrayList<>();
 
         for (int sheet = 0; sheet < board.numSheets(); sheet++) { //Go through all the spots on the board
             for (int row = 0; row < board.numRows(); row++) {
@@ -62,9 +79,9 @@ public class RecursivePython implements PlayerInt {
                     if (board.isEmpty(new Location(sheet, row, col))) { //If this spot is valid, ie not taken
                         Location locCurrent = new Location(sheet, row, col);
 
-                        LocationScore ls = new LocationScore(board, locCurrent, letter);
+                        LocationScore ls = new LocationScore(board, locCurrent, letter); //gets the location score of the current spot
 
-                        if (ls.getOtherQuadruples() > 0) {
+                        if (ls.getOtherQuadruples() > 0) { //add the location to one of the arraylists for the enemy if the spot is good for them
                             otherQuadruples.add(locCurrent);
                         } else if (ls.getOtherTriples() > 0) {
                             otherTriples.add(locCurrent);
@@ -76,7 +93,7 @@ public class RecursivePython implements PlayerInt {
                             otherZeros.add(locCurrent);
                         }
 
-                        if (ls.getSelfQuadruples() > 0) {
+                        if (ls.getSelfQuadruples() > 0) { //add the location to one of the self arrayLists if spot is good for us
                             selfQuadruples.add(locCurrent);
                         } else if (ls.getSelfTriples() > 0) {
                             selfTriples.add(locCurrent);
@@ -89,7 +106,6 @@ public class RecursivePython implements PlayerInt {
                         }
 
                         score = (ls.getSelfQuadruples() * 1000) + (ls.getSelfTriples() * 100) + (ls.getSelfDoubles() * 10) + ls.getSelfSingles();
-                        otherScore = (ls.getOtherQuadruples() * 1000) + (ls.getOtherTriples() * 100) + (ls.getOtherDoubles() * 10) + ls.getOtherSingles();
 
                         //System.out.println("(" + sheet + "," + row + "," + col + ")  My score here is..." + score);
                         if (score > scoreTemp) { //If this location is the best so far
@@ -101,8 +117,9 @@ public class RecursivePython implements PlayerInt {
             }
         }
 
+        //Logic for how to move ----------------------------------------------------
+        //todo after this is done, old method with score should be unnecessary
         //if()
-
         //System.out.println("I moved to..." + loc);
         return locFinal;
     }
