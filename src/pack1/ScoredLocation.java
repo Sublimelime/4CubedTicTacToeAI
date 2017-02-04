aed67731e3c1b971e56be8a46d4cef526a45fbf6
Noah Morton <noahmorton98@gmail.com>
2017-02-04 14:25:37 -0600

    Made ScoredLocation comparable.

--- src/pack1/ScoredLocation.java
+++ src/pack1/ScoredLocation.java
@@ -12,7 +12,7 @@ import tully.LocationInt;
  *
  * Part of project: TicTacToeAI
  */
-public class ScoredLocation extends Location implements LocationInt {
+public class ScoredLocation extends Location implements LocationInt, Comparable<ScoredLocation> {
 
     private int scoreOfLocation;
 
@@ -36,13 +36,54 @@ public class ScoredLocation extends Location implements LocationInt {
     }
 
     /**
-     * Changes the class back into a regular location, so it interfaces with the AI dispatcher.
+     * Changes the class back into a regular location, so it interfaces with the
+     * AI dispatcher.
      *
-     * @return A new Location with the sheet,row, and col of the current location.
+     * @return A new Location with the sheet,row, and col of the current
+     * location.
      * @see tully.Location
      */
     public Location makeLocationOnly() {
         return new Location(getSheet(), getRow(), getCol());
     }
 
+    /**
+     * Compare two scored locations, works similarly to a standard compareTo.
+     *
+     * @param o A ScoredLocation to compare to 'this'
+     * @return -1, 0, or 1 depending on if the provided object's score is
+     * greater or smaller.
+     */
+    @Override
+    public int compareTo(ScoredLocation o) {
+        return (scoreOfLocation < o.getScoreOfLocation()) ? -1 : (scoreOfLocation > o.getScoreOfLocation()) ? 1 : 0;
+    }
+
+    /**
+     * Compares 'this' and the provided Object for their scores. They are
+     * considered equal if they have the same memory address, or the same score.
+     *
+     * @param o Object to check for equivalency.
+     * @return True if they are both ScoredLocations and the scores match. _Does
+     * not compare location._
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { //they are the same memory address
+            return true;
+        } else if (o instanceof ScoredLocation) {
+            ScoredLocation temp = (ScoredLocation) o;
+            if (temp.getScoreOfLocation() == this.scoreOfLocation) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + this.scoreOfLocation;
+        return hash;
+    }
 }
